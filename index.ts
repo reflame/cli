1e7b157994e8773387da6966b7d0d3b4f6139320
Lewis Liu <lewisl9029@users.noreply.github.com>
2023-11-19 20:51:20 -0800

    Update

--- index.ts
+++ index.ts
@@ -7,14 +7,58 @@ import * as libResource_ from "@reflame/lib-resource";
 (async () => {
   const workingDirectory = process.cwd();
 
-  const token = process.env.GITHUB_TOKEN;
-  console.log({ token });
-
   const config = jsoncParser_.parse(
     await fs_.readFile(path_.join(workingDirectory, ".reflame.config.jsonc"), {
       encoding: "utf-8",
     })
   );
+
+  const secret = process.env.REFLAME_SECRET;
+  const githubToken = process.env.GITHUB_TOKEN;
+
+  const installationsPayload = await fetch(
+    "https://api.github.com/user/installations",
+    {
+      method: "GET",
+      headers: {
+        accept: "application/vnd.github.v3+json",
+        authorization: `token ${githubToken}`,
+      },
+    }
+  ).then((response) => response.json());
+
+  console.log({ installationsPayload });
+
+  if (!secret) {
+    throw new Error("missing REFLAME_SECRET environment variable");
+  }
+
+  const appId = config.appId;
+
+  console.log({ appId, secret });
+
+  const { payload } = await fetch(
+    "https://identity.reflame.cloud/cli/exchange-secret",
+    {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        appId,
+        secret,
+      }),
+    }
+  ).then(async (response) => ({
+    headers: response.headers,
+    payload: await response.json(),
+  }));
+
+  const accessToken = payload.accessToken;
+
+  if (!accessToken) {
+    throw new Error("failed to get access token");
+  }
 
   // TODO: prep npm package bundle ahead of time
 
@@ -50,7 +94,30 @@ import * as libResource_ from "@reflame/lib-resource";
     .filter((resource) => !!resource)
     .sort((a, b) => (a.pathname < b.pathname ? -1 : 1));
 
-  const resourceIds = resources.map(({ id }) => id);
+  const resourcesWithoutData = resources.map(
+    ({ data, ...resource }) => resource
+  );
 
-  console.log(resourceIds);
+  console.log(resourcesWithoutData);
+  const { resourceMissingByPathnameApp } = await fetch(
+    "https://deployer.reflame.cloud/cli/get-resources-missing",
+    {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify({
+        appId,
+        resourcesWithoutData,
+      }),
+    }
+  ).then((response) => response.json());
+
+  // TODO: need to save installation id for each app installed with the minimal-access github app
+  // can eagerly update during installation process?
+  // Might also need to add single file access to reflame config to identify app id
+  // if we need to do this exclusively through webhooks...
+
+  console.log(resourceMissingByPathnameApp);
 })();
