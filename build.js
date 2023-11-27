3856463ad99cea7219d972a123dccbea20de41a3
Lewis Liu <lewisl9029@users.noreply.github.com>
2023-11-26 22:34:29 -0800

    Attempt npx fix

--- build.js
+++ build.js
@@ -35,6 +35,14 @@ await Promise.all([
     define: {
       "process.env.NODE_ENV": `"production"`,
     },
+    banner: {
+      //https://github.com/evanw/esbuild/issues/1921
+      js: `
+      const require = (await import("node:module")).createRequire(import.meta.url);
+      const __filename = (await import("node:url")).fileURLToPath(import.meta.url);
+      const __dirname = (await import("node:path")).dirname(__filename);
+      `,
+    },
     plugins: [jsoncParserPlugin],
     format: "esm",
     outdir: "build",
